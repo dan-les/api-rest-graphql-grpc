511c1a25e397c34c066fe4ce609cf3c183941b79
rootuss <rootuss@op.pl>
2023-02-18 16:24:24 +0100

    added logger for REST::getAllFilms()

--- src/main/java/com/lesniewicz/api/controler/rest/FilmController.java
+++ src/main/java/com/lesniewicz/api/controler/rest/FilmController.java
@@ -4,12 +4,14 @@ package com.lesniewicz.api.controler.rest;
 import com.lesniewicz.api.dto.FilmDto;
 import com.lesniewicz.api.service.FilmService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/rest", produces = "application/json")
@@ -20,6 +22,7 @@ public class FilmController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public List<FilmDto> getAllFilms() {
+        log.info("REST::getAllFilms()");
         return filmService.getAllFilms();
     }
 
